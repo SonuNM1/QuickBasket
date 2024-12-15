514f17a93e8d0d611662e4a4a146623f14c35765
sonunm1 <sonu.mahto362000@gmail.com>
2024-12-15 20:06:19 +0530

    third part done

--- client/src/pages/ProductAdmin.jsx
+++ client/src/pages/ProductAdmin.jsx
@@ -6,12 +6,16 @@ import AxiosToastError from "../util/AxiosToastError";
 import Axios from "../util/Axios";
 import Loading from "../components/Loading";
 import ProductCardAdmin from "../components/ProductCardAdmin";
+import {IoSearchOutline} from 'react-icons/io5'
+
+
 
 const ProductAdmin = () => {
   const [productData, setProductData] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
-  const [totalPageCount, setTotalPageCount] = useState(1)
+  const [totalPageCount, setTotalPageCount] = useState(1);
+  const [search, setSearch] = useState("")
 
   const fetchProductData = async () => {
     try {
@@ -21,7 +25,8 @@ const ProductAdmin = () => {
         ...SummaryApi.getProduct,
         data: {
           page: page,
-          limit: 12
+          limit: 12,
+          search: search
         },
       });
 
@@ -41,46 +46,88 @@ const ProductAdmin = () => {
     fetchProductData();
   }, [page]);
 
-  const handleNext = () =>{
-    if(page !== totalPageCount){
-      setPage(prev => prev+1)
+  const handleNext = () => {
+    if (page !== totalPageCount) {
+      setPage((prev) => prev + 1);
     }
+  };
+
+  const handlePrevious = () => {
+    if (page > 1) {
+      setPage((prev) => prev - 1);
+    }
+  };
+
+  const handleOnChange = (e) => {
+    const {value} = e.target
+    setSearch(value)
+    setPage(1)
   }
 
-  const handlePrevious = () =>{
-    if(page > 1){
-      setPage(prev => prev-1)
+  // concept of debouncing used to reduce API hit
+
+  useEffect(()=>{
+
+    let flag = true 
+
+    const interval = setTimeout(() => {
+      if(flag){
+        fetchProductData()
+        flag = false 
+      }
+    }, 300);
+
+    return ()=>{
+      clearTimeout(interval)
     }
-  }
+
+  },[search])
 
   return (
     <section className="">
-      <div className="p-2 bg-white shadow-md flex items-center justify-between">
-        <h2 className="font-semibold">Product</h2>
-      </div>
+      <div className="p-2 bg-white shadow-md flex items-center justify-between gap-4">
+          <h2 className="font-semibold">Product</h2>
+          <div className="h-full  min-w-24 w-full max-w-56 ml-auto bg-blue-50 px-4 flex items-center gap-2 py-2  rounded border focus-within:border-primary-200">
+            <IoSearchOutline size={25}/>
+            <input
+              type="text"
+              placeholder="Enter product here ..."
+              className="h-full w-full outline-none bg-transparent"
+              value={search}
+              onChange={handleOnChange}
+            />
+          </div>
+        </div>
       {loading && <Loading />}
 
       <div className="p-4 bg-blue-50 grid ">
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
-          {productData.map((p, index) => {
-            return <ProductCardAdmin data={p} />;
-          })}
+        <div className="min-h-[55vh]">
+          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 ">
+            {productData.map((p, index) => {
+              return <ProductCardAdmin data={p} />;
+            })}
+          </div>
         </div>
 
         <div className="flex justify-between my-4">
+          <button
+            onClick={handlePrevious}
+            className="border border-primary-200 px-4 py-1 hover:bg-primary-200"
+          >
+            Previous
+          </button>
 
-          <button 
-          onClick={handlePrevious}
-          className="border border-primary-200 px-4 py-1 hover:bg-primary-200">Previous</button>
-        
-          <button className="w-full bg-slate-100">{page}/{totalPageCount}</button>
+          <button className="w-full bg-slate-100">
+            {page}/{totalPageCount}
+          </button>
 
-          <button 
-          onClick={handleNext}
-          className="border border-primary-200 px-4 py-1 hover:bg-primary-200">Next</button>
-
-        </div>  
-
+          <button
+            onClick={handleNext}
+            className="border border-primary-200 px-4 py-1 hover:bg-primary-200"
+          >
+            Next
+          </button>
+        </div>
       </div>
     </section>
   );
